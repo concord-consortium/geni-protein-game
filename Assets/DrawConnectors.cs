f6f1e3102f8a0adcc52df929c53c1aee13faa7e5
Chris Hart <chart@concord.org>
2016-07-26 15:00:59 -0400

    Hide hinge joint link sprites at run time and replace with a smoother line renderer component

---
+++ Assets/DrawConnectors.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawConnectors : MonoBehaviour {
+
+  	public LineRenderer lineRenderer;
+	public Transform[] connectorJoints;
+	private List<Vector3> connectorPositions;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = this.gameObject.GetComponent<LineRenderer> ();
+		connectorPositions = new List<Vector3>();
+		for (var i = 0; i < connectorJoints.Length; i++)
+		{
+			connectorPositions.Add(connectorJoints[i].position);
+			var sprite = connectorJoints [i].gameObject.GetComponent<SpriteRenderer> ();
+			if (sprite != null) {
+				Debug.Log ("disabling sprite");
+				sprite.enabled = false;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (var i = 0; i < connectorJoints.Length; i++)
+		{
+			connectorPositions[i] = connectorJoints[i].position;
+		}
+	}
+
+	void FixedUpdate () {
+		lineRenderer.SetPositions (connectorPositions.ToArray());
+	}
+}
